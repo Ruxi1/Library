024568a93a7114ea92ff79c3d616d3b8d6e5bfae
Ruxi1 <ruxandramaria.baros@ulbsibiu.ro>
2024-04-08 12:10:18 +0300

    FirstUnitTest

---
+++ LibraryTests/LoanTest.cs
@@ -0,0 +1,41 @@
+﻿using Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class LoanTest
+    {
+
+        
+        [TestMethod]
+        public void HasManyUnreturnedBooks_Test1_Morebooks()
+        {
+            Loan loan=new Loan();
+            Client client = new Client("Ruxi", "ruxy@gmail.com", true, 2);
+            bool result = loan.HasManyUnreturnedBooks(client, 1);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasManyUnreturnedBooks_Test2_LessBooks()
+        {
+            Loan loan=new Loan();
+            Client client = new Client("Ruxi", "ruxy@gmail.com", true, 2);
+            bool result = loan.HasManyUnreturnedBooks(client, 4);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void HasManyUnreturnedBooks_Test3_EqualNumberOfBooks()
+        {
+            Loan loan = new Loan();
+            Client client = new Client("Ruxi", "ruxy@gmail.com", true, 2);
+            bool result = loan.HasManyUnreturnedBooks(client, 2);
+            Assert.IsFalse(result);
+        }
+    }
+}
